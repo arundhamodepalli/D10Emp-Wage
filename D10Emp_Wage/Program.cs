c6af372a39ea9a8656e5a2b1c175decece304f0c
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-10 20:48:50 +0530

    welcome o employee wage computation

---
+++ D10Emp_Wage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D10Emp_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage computation");
+        }
+    }
+}
